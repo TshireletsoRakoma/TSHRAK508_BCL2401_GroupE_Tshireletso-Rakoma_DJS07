9f0e57f059debe4acd03b2f4b0119ca1a4f6b94d
TshireletsoRakoma <156686384+TshireletsoRakoma@users.noreply.github.com>
2024-05-31 15:00:23 +0200

    "Added Header and InputFields components to App.jsx, updated index.css with new styles, and made no changes to main.jsx except for an empty line addition."

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Header from "./COMPONENTS/Header"
+import InputFields from "./COMPONENTS/Inputfield"
 
 function App() {
 
   return (
     <>
-     
+     <div>
+      <Header/>
+      <InputFields/>
+     </div>
     </>
   )
 }
---
+++ src/COMPONENTS/Header.jsx
@@ -0,0 +1,15 @@
+
+
+const Header = () => {
+    return (
+        <>
+            <header className="header">
+                <img src="troll-face.png" className="header--image" alt="Troll Face"></img>
+                <h2 className="header--title">Meme Generator</h2>
+                <h4 className="header--project">DJS07 - Project</h4>
+            </header>
+        </>
+    );
+};
+
+export default Header;
